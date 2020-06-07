cd3652bf6acb6b1060205bdfd4f436d878a4f6e7
Nathan Rowbottom <nathan.rowbottom@snpsteam.com>
2020-06-07 14:38:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var vid = document.getElementById("test");
+console.log("test"+vid);
+
+
+   vid.addEventListener( 'click', function ( ){
+    /*vid.setAttribute( 'scale',
+    {
+      x: 4, y: 7, z: 6
+    } );*/
+    vid.play()
+} );
